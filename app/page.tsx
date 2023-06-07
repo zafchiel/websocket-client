de8c7a802e638bcf992f48eb6e50dd4ff147e8fc
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-06-07 20:30:19 +0200

    'sync'

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
+"use client"
+
+import { FormEvent, useState } from "react"
+import { io } from "socket.io-client"
+
 export default function HomePage() {
+  const [message, setMessage] = useState("")
+  const socket = io("http://localhost:3001")
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    socket.emit("chat message", message)
+    setMessage("")
+  }
+
   return (
-    <main className="w-full h-screen flex items-center justify-center">
-      <h1>Hello</h1>
+    <main className="flex h-screen w-full items-center justify-center">
+      <div className="flex flex-col text-black">
+        <div className="h-80 w-80 bg-gray-500"></div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit" className="bg-gray-500">
+            Send
+          </button>
+        </form>
+      </div>
     </main>
   )
 }
