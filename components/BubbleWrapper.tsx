1de8bb7fe3dba623c4c8ebb04df8e7fa4286806b
Zafchiel <bialakropla127.9@o2.pl>
2023-06-24 17:41:21 +0200

    functional bubble divs

--- components/BubbleWrapper.tsx
+++ components/BubbleWrapper.tsx
@@ -1,9 +1,30 @@
+import { MouseEvent, useEffect, useRef } from "react"
+import { cn } from "@/lib/utils"
 import styles from "./bubbles.module.css"
 
 export default function BubbleWrapper() {
-  return (
-    <div className="fixed inset-y-0 left-0">
-      <div className={styles.bubble}></div>
-    </div>
-  )
+  const wrapperRef = useRef<HTMLDivElement | null>(null)
+
+  const handleMouse = (event: MouseEvent<HTMLDivElement>) => {
+    const bubble: HTMLDivElement = createBubble(event.clientY)
+    wrapperRef.current?.appendChild(bubble)
+
+    setTimeout(() => {
+      wrapperRef.current?.removeChild(bubble)
+    }, 2000)
+  }
+
+  const createBubble = (y: number) => {
+    const bubble: HTMLDivElement = document.createElement("div")
+    bubble.className = styles.bubble
+    bubble.style.top = `${y}px`
+
+    return bubble
+  }
+
+  useEffect(() => {
+    window.onmousemove = (e: any) => handleMouse(e)
+  }, [])
+
+  return <div ref={wrapperRef} className="fixed inset-y-0 left-0"></div>
 }
