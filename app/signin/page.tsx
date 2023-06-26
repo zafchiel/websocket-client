ab3607dedc97bb59ce228469214f5c07763de157
Zafchiel <bialakropla127.9@o2.pl>
2023-06-26 23:08:24 +0200

    OAuth icons

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,13 +1,23 @@
 import SignInButton from "@/components/SignInButton"
+import Google from "@/assets/icons/Google"
+import Reddit from "@/assets/icons/Reddit"
 
 export default function SignInPage() {
   return (
     <main className="flex h-screen w-full items-center justify-center">
-      <section className="flex w-44 flex-col items-center justify-center gap-5">
-        <SignInButton provider="google" className="w-full">
+      <section className="flex w-52 flex-col items-center justify-center gap-5">
+        <SignInButton
+          provider="google"
+          className="flex w-full items-center justify-center gap-2"
+        >
+          <Google size={24} />
           Sign-In with Google
         </SignInButton>
-        <SignInButton provider="reddit" className="w-full">
+        <SignInButton
+          provider="reddit"
+          className="flex w-full items-center justify-center gap-2"
+        >
+          <Reddit size={25} />
           Sign-In with Reddit
         </SignInButton>
       </section>
